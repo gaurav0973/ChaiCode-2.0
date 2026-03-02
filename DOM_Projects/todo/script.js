64838bfa593313c1d9b981316ca0076aa0dc1192
gaurav0973 <codejourneypanda@gmail.com>
2026-03-02 16:58:26 +0530

    started project: todo list => basic feature build

---
+++ DOM_Projects/todo/script.js
@@ -0,0 +1,9 @@
+const addTodoBtn = document.getElementById("addBtn")
+const inputField = document.querySelector("#inputArea")
+const todoList = document.querySelector(".todoList")
+// console.log("scerip ti loaded")
+
+document.addEventListener("click", function(){
+    const value = inputField.value
+    console.log(value)
+})
