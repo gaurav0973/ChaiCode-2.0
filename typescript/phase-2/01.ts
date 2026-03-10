87f4742479caea9beeac9bab3aeae602a4948d26
gaurav0973 <codejourneypanda@gmail.com>
2026-03-10 16:09:19 +0530

    learn : interface and how it is different from type

---
+++ typescript/phase-2/01.ts
@@ -0,0 +1,54 @@
+// function printUser(user){
+//     console.log(`User : ${user.name} (${user.email})`)
+// }
+// printUser({
+//   id: 1,
+//   name: "Gaurav",
+//   email: "gaurav@gmail.com"
+// })
+
+
+// interface User {
+//     id : number
+//     name: string
+//     email: string
+// }
+// function printUser(user: User){
+//     console.log(`User : ${user.name} (${user.email})`)
+// }
+// printUser({
+//   id: 1,
+//   name: "Gaurav",
+//   email: "gaurav@gmail.com"
+// })
+
+//     interface User {
+//     id: number
+//     name: string
+//     }
+//     interface Admin extends User{
+//         role : string
+//     }
+
+//     const admin : Admin = {
+//     id: 1,
+//     name: "Gaurav",
+//     role: "superadmin"
+//     }
+
+
+// interface User {
+//   readonly id: number // this cannot be modefied 
+//   name: string
+// }
+
+// const user: User = {
+//   id : 1,
+//   name: "Gaurav"
+// }
+
+// // user.id = 99
+
+
+
+
