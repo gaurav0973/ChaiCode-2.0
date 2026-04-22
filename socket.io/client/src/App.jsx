3386fcaebf87d903fe4b02e6819b33a39b202b2c
gaurav0973 <codejourneypanda@gmail.com>
2026-04-22 18:44:22 +0530

    socketio

---
+++ socket.io/client/src/App.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:4000");
+
+export default function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    socket.on("receive_message", (msg) => {
+      setChat((prev) => [...prev, msg]);
+    });
+  }, []);
+
+  const sendMessage = () => {
+    if (message.trim() === "") 
+      return;
+    socket.emit("send_message", message);
+    setMessage("");
+  };
+
+  return (
+    <div>
+      <h2>Simple Chat Application</h2>
+
+      {/* Messages */}
+      <div>
+        {chat.map((msg, i) => (
+          <p key={i}>{msg}</p>
+        ))}
+      </div>
+
+      {/* Input */}
+      <input
+        type="text"
+        placeholder="Type message..."
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  );
+}
---
+++ socket.io/server/index.js
@@ -0,0 +1,33 @@
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import cors from "cors";
+import express from "express";
+
+async function main() {
+    const app = express();
+    app.use(cors({ origin: "*" }));
+    const server = createServer(app);
+
+    const io = new Server(server, {
+        cors: {
+            origin: "*"
+        }
+    });
+
+    io.on("connection", (socket) => {
+        console.log("A user connected:", socket.id);
+        socket.on("send_message", (message) => {
+            io.emit("receive_message", message);
+        });
+
+        socket.on("disconnect", () => {
+            console.log("User disconnected:", socket.id);
+        });
+    });
+
+    server.listen(4000, () => {
+        console.log("Server is listening on port 4000");
+    });
+}
+
+main();
